2d505f176068945feed5966653cb0527bd44b2e1
unknown <ayoubnasraoui161@yahoo.fr>
2023-09-03 12:49:33 +0200

    Style Buttons Element state and transition

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,7 +3,7 @@ import SearchOrder from "../features/order/SearchOrder";
 import Username from "../features/user/Username";
 export default function Header() {
   return (
-    <header className="border-b-8 border-stone-200 bg-yellow-500 px-4 py-3 uppercase  sm:px-6 flex items-center justify-between">
+    <header className="border-b-8 border-stone-200 bg-yellow-400 px-4 py-3 uppercase  sm:px-6 flex items-center justify-between">
       <Link to="/" className="tracking-widest">
         Fast React Pizza Co.
       </Link>
