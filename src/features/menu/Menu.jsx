f7e924342f1c1cd970c7bf0be0d90b3bafabac46
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2023-10-16 20:58:29 +0100

    Styling the cart

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -3,7 +3,7 @@ import { getMenu } from "../../services/apiRestaurant";
 import MenuItem from "./MenuItem"
 function Menu() {
  const menu= useLoaderData();
-  return <ul>{menu.map(pizza=>{
+  return <ul className="divide-y divide-stone-200 px-2">{menu.map(pizza=>{
     return (
       <MenuItem pizza={pizza} key={pizza.id}/>
       )
