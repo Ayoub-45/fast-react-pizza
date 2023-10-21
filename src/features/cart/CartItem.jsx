6035da89276676e2f11bc1790338cd6e5a5c42a4
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2023-10-21 07:40:55 +0100

    Delete Cart ITem

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,16 +1,16 @@
-import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
+import DeleteItem from "./DeleteItem";
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
-
+  console.log(pizzaId)
   return (
-    <li  className="py-3 sm:flex sm:items-center sm:justify-between">
+    <li key={pizzaId} className="py-3 sm:flex sm:items-center sm:justify-between">
       <p className="mb-1  sm:mb-0 ">
         {quantity}&times; {name}
       </p>
       <div className="flex items-center justify-between gap-6">
         <p>{formatCurrency(totalPrice)}</p>
-        <Button type="small">Delete</Button>
+      <DeleteItem pizzaId={pizzaId}/>
       </div>
     </li>
   );
--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,10 +1,16 @@
 import { formatCurrency } from "../../utils/helpers";
 import Button from "../../ui/Button";
 import { useDispatch } from "react-redux";
+import {useSelector} from "react-redux"
 import { addItem } from "../cart/cartSlice";
+import { deleteItem } from "../cart/cartSlice";
+import { getCurrentCartQuantityById } from "../cart/cartSlice";
+import DeleteItem from "../cart/DeleteItem";
 function MenuItem({ pizza }) {
   const dispatch=useDispatch();
   const {  id,name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const currentQuantity=useSelector(getCurrentCartQuantityById(id))
+ const isInCart=currentQuantity>0
   function handleAddToCart(){
    const newItem={
     pizzaId:id,
@@ -22,9 +28,11 @@ function MenuItem({ pizza }) {
         <p className="font-medium">{name}</p>
         <p className="text-sm italic text-stone-500 capitalize">{ingredients.join(', ')}</p>
         <div className="mt-auto flex items-center justify-between">
+        
           {!soldOut ? <p className="text-sm">{formatCurrency(unitPrice)}</p> : <p className="text-sm uppercase font-medium text-stone-500">Sold out</p>}
+        { isInCart && <DeleteItem pizzaId={id}/>}
         {
-          !soldOut &&
+          !soldOut &&!isInCart &&
           <Button type="small" onClick={handleAddToCart}>Add to cart</Button>
         }
         </div>
