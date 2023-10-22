ed54b45c6e36ffc09c21fd460201d1c491e3aaf0
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2023-10-22 09:58:30 +0100

    Integrating geolocation the app

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -21,7 +21,9 @@ function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
   const formErrors = useActionData();
-  const username=useSelector(state=>state.user.username);
+  const {username,status:addressStatus,position,address,error:errorAddress}=useSelector(state=>state.user);
+  console.log(address)
+  const isLoadingAddress=addressStatus==="loading"
   const cart =useSelector(getCart)
   const totalCartPrice=useSelector(getTotalCartPrice)
   const priorityPrice=withPriority?totalCartPrice*0.2:0;
@@ -31,7 +33,7 @@ function CreateOrder() {
   return (
     <div className="px-4 py-4">
       <h2 className="text-xl font-semibold mb-8">Ready to order? Let's go!</h2>
-      <button onClick={()=>dispatch(fetchAddress())}>Get position</button>
+     
       <Form method="POST">
         <div className="mb-5 flex gap-2 flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
@@ -49,16 +51,36 @@ function CreateOrder() {
           {formErrors?.phone && <p className="text-xs mt-2 text-red-200 bg-red-700 p-2 rounded">{formErrors.phone}</p>}
         </div>
 
-        <div className="mb-5 flex gap-2 flex-col gap-2 sm:flex-row sm:items-center">
+        <div className="relative mb-5 flex gap-2 flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Address</label>
           <div className="grow">
             <input
               type="text"
               className="focus-outline-none w-full rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:ring focus:ring-yellow-400 md:px-6 md:py-3"
               name="address"
+              disabled={isLoadingAddress}
+              defaultValue={address}
               required
             />
+            {addressStatus ==="error" && (
+              (<p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                {errorAddress}
+              </p>)
+            )}
           </div>
+          {!position.latitude && ! position.longitude &&
+          (
+            <span className="absolute right-[3px]">
+          <Button  
+          disabled={isLoadingAddress}
+          type="small" 
+          onClick={(e)=>{
+            e.preventDefault()
+            dispatch(fetchAddress())
+          }}>Get position</Button>
+          </span>
+          )
+        }
         </div>
 
         <div className="mb-12 flex items-center gap-5">
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -14,9 +14,8 @@ export const fetchAddress=createAsyncThunk("user/fetchAddress",async function(){
   };
 
   // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
-  const addressObj = await getPosition(position);
+  const addressObj = await getAddress(position);
   const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
-
   // 3) Then we return an object with the data that we are interested in
   return { position, address }
 })
@@ -48,7 +47,7 @@ const userSlice=createSlice({
     })
     .addCase(fetchAddress.rejected,(state,action)=>{
       state.status="error";
-      state.error=action.error.message;
+      state.error="There was problem getting your address, make sure to fill this field";
     })
 })
 export const {updateName}=userSlice.actions;
