85eae2d3be12ae00da91e89814f995458d1aa654
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2023-10-22 08:14:05 +0100

    Use cart for new  orders

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,43 +1,30 @@
+import { useState } from "react";
 import { useSelector } from "react-redux/es/hooks/useSelector";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
+import EmptyCart from "../cart/EmptyCart"
+import { getCart, getTotalCartPrice } from "../cart/cartSlice";
+import store from "../../store"
+import { clearCart } from "../cart/cartSlice";
+import { formatCurrency } from "../../utils/helpers";
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
     str,
   );
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "Vegetale",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
 
 function CreateOrder() {
-  // const [withPriority, setWithPriority] = useState(false);
-  const cart = fakeCart;
+  const [withPriority, setWithPriority] = useState(false);
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
   const formErrors = useActionData();
   const username=useSelector(state=>state.user.username);
+  const cart =useSelector(getCart)
+  const totalCartPrice=useSelector(getTotalCartPrice)
+  const priorityPrice=withPriority?totalCartPrice*0.2:0;
+  const totalPrice=totalCartPrice+priorityPrice
+  if(!cart.length) return <EmptyCart/>
   return (
     <div className="px-4 py-4">
       <h2 className="text-xl font-semibold mb-8">Ready to order? Let's go!</h2>
@@ -45,7 +32,7 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex gap-2 flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
- 
+  
           <div className="grow">
           <input type="text" className="focus-outline-none w-full rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:ring focus:ring-yellow-400 md:px-6 md:py-3" name="customer" required defaultValue={username} />
         
@@ -82,8 +69,8 @@ function CreateOrder() {
             name="priority"
             id="priority"
 
-            // value={withPriority}
-            // onChange={(e) => setWithPriority(e.target.checked)}
+            value={withPriority}
+            onChange={(e) => setWithPriority(e.target.checked)}
           />
           <label htmlFor="priority">Want to yo give your order priority?</label>
         </div>
@@ -91,7 +78,7 @@ function CreateOrder() {
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
           <Button disabled={isSubmitting}>
-            {isSubmitting ? "placing order ..." : "Order now"}
+            {isSubmitting ? "placing order ..." : `Order now from ${formatCurrency(totalPrice)} `}
           </Button>
         </div>
       </Form>
@@ -105,16 +92,16 @@ export async function action({ request }) {
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === "on",
+    priority: data.priority === "true",
   };
   const errors = {};
   if (!isValidPhone(order.phone))
-    errors.phone =
-      "Please give us your correct phone number, we might need it to contact you. ";
+    errors.phone ="Please give us your correct phone number, we might need it to contact you. ";
   if (Object.keys(errors).length > 0) return errors;
   //If everything is ok ,create new order and redirect
   const newOrder = await createOrder(order);
+  store.dispatch(clearCart());
   return redirect(`/order/${newOrder.id}`);
 }
 
-export default CreateOrder;
+export default CreateOrder; 
